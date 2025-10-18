8fff72ab670fa9721fd73deef0f5b3bac2a64958
JuanVergara-9 <juanvergara920@gmail.com>
2025-10-18 12:52:00 -0300

    logo

--- app/metadata.tsx
+++ app/metadata.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   icons: {
     icon: [
-      { url: '/favicon.ico' },
-      { url: '/icon.svg', type: 'image/svg+xml' },
+      { url: '/logo.png' },
+      { url: '/favicon.ico' }
     ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180' },
